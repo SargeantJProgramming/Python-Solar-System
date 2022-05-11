b6b10b54061d88c6cb6bb4d508e85be70572146f
MrSargeant <104493782+SargeantJProgramming@users.noreply.github.com>
2022-05-11 13:27:33 +0000

    Working towards creating a functioning dependent orbiting system using object calls and functions

--- SpaceObject.py
+++ SpaceObject.py
@@ -1,17 +1,24 @@
+import math
+
 class SpaceObject():
-  orbitCentre = [0,0]
+  orbitCentre = (0,0)
   orbitRadius = 0
   objectRadius = 0
   speed = 0
+  colour = (0,0,0)
+  currentCoordinates = (0,0)
   
-  def __init__(self,objectRadius, orbitCentre,orbitRadius,speed):
+  def __init__(self,objectRadius, orbitCentre,orbitRadius,speed,colour):
     self.objectRadius = objectRadius
     self.orbitCentre = orbitCentre
     self.orbitRadius = orbitRadius
+    self.colour = colour
     self.speed = speed
 
   def updateRadius(self,orbitCentre,orbitRadius):
     self.orbitCentre = orbitCentre
     self.orbitRadius = orbitRadius
 
+  def velocity(self):
+    velocity = self.speed*self.orbitRadius*math.pi
     
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys, pygame
+from SpaceObject import SpaceObject
 
 pygame.init()
 
@@ -6,9 +7,19 @@ height = 400
 width = 400
 size = [height,width]
 
-centre = [height/2,width/2]
+centre = (height/2,width/2)
 space = pygame.display.set_mode(size)
-pygame.draw.circle(space,[255,200,0],centre,10)
+#pygame.draw.circle(space,[255,200,0],centre,10)
+#pygame.display.update()
+#self,objectRadius, orbitCentre,orbitRadius,speed,colour 
+sun = SpaceObject(10,centre,0,10,(255,200,0))
+#print(sun.colour,sun.orbitCentre,sun.objectRadius)
+pygame.draw.circle(space,sun.colour,sun.orbitCentre,sun.objectRadius)
 pygame.display.update()
 
+moon = SpaceObject(5,(sun.currentCoordinates[0],sun.currentCoordinates[1]),10,10,(150,150,150))
 
+print((sun.currentCoordinates[0],sun.currentCoordinates[1]),10,10,(150,150,150))
+print(moon.colour,moon.orbitCentre,moon.objectRadius)
+pygame.draw.circle(space,moon.colour,moon.orbitCentre,moon.objectRadius)
+pygame.display.update()
